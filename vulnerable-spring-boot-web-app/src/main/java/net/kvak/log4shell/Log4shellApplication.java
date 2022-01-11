56189fbc9b8db5bc3aa14d3a85951bf7bd24793b
Alexander C <alexander.culum@gmail.com>
2022-01-11 14:46:33 +0100

    Directly set JNDI Lookup properties in main method for later versions. Add OWASP dependency check.

--- vulnerable-spring-boot-web-app/src/main/java/net/kvak/log4shell/Log4shellApplication.java
+++ vulnerable-spring-boot-web-app/src/main/java/net/kvak/log4shell/Log4shellApplication.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Log4shellApplication {
 
   public static void main(String[] args) {
+    // For versions later than JRE/JDK 11.0.1, 8u191, 7u201 and 6u211, the property has to explicitly set to true
+    System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase", "true");
     SpringApplication.run(Log4shellApplication.class, args);
   }
-
 
 }
